ef2ef6c9f2dbeeaa0a7b057ba3dc57946b435dc1
pythogoras570 <133265951+pythogoras570@users.noreply.github.com>
2023-09-14 23:51:19 +0300

    Added full code.

--- PAdvanced/Lectures/Lists_as_Stacks_and_Queues/matching_parentheses.py
+++ PAdvanced/Lectures/Lists_as_Stacks_and_Queues/matching_parentheses.py
@@ -1,10 +1,9 @@
 expression = input()
-
 stack = []
 
 for index in range(len(expression)):
     if expression[index] == '(':
-        stack.append(expression[index])
+        stack.append(index)
     elif expression[index] == ')':
         start_index = stack.pop()
         print(expression[start_index:index+1])
