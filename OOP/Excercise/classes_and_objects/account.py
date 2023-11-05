39a1374f046a0928bcbb75f96ec52d2ef3775329
pythogoras570 <133265951+pythogoras570@users.noreply.github.com>
2023-11-05 22:43:25 +0200

    Added full code.

--- OOP/Excercise/classes_and_objects/account.py
+++ OOP/Excercise/classes_and_objects/account.py
@@ -6,10 +6,14 @@ class Account:
         self.balance = balance
 
     def credit(self, amount):
-        pass
+        self.balance += amount
+        return self.balance
 
-    def debit(self,amount):
-        pass
+    def debit(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            return self.balance
+        return "Amount exceeded balance"
 
     def info(self):
-        return f"User {name} with account {id} has {balance} balance"
+        return f"User {self.name} with account {self.id} has {self.balance} balance"
