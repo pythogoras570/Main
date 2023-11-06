b7f3508d6808ba62bbe216c5e9506f714cb48301
pythogoras570 <133265951+pythogoras570@users.noreply.github.com>
2023-11-07 00:10:40 +0200

    Added full code.

--- OOP/Excercise/classes_and_objects/pizza_delivery.py
+++ OOP/Excercise/classes_and_objects/pizza_delivery.py
@@ -10,10 +10,25 @@ class PizzaDelivery:
         self.ordered = False
 
     def add_extra(self, ingredient: str, quantity: int, price_per_quantity: float):
-        pass
+        if self.ordered:
+            return f"Pizza {self.name}ac already prepared, and we can't make any changes!"
+        if ingredient not in self.ingredients:
+            self.ingredients[ingredient] = 0
+        self.ingredients[ingredient] += quantity
+        self.price += quantity * price_per_quantity
 
     def remove_ingredient(self, ingredient: str, quantity: int, price_per_quantity: float):
-        pass
+        if self.ordered:
+            return f"Pizza {self.name} already prepared, and we can't make any changes!"
+        if ingredient not in self.ingredients:
+            return f"Wrong ingredient selected! We do not use {ingredient} in {self.name}!"
+        if self.ingredients[ingredient] < quantity:
+            return f"Please check again the desired quantity of {ingredient}!"
+        self.ingredients[ingredient] -= quantity
+        self.price -= quantity * price_per_quantity
 
     def make_order(self):
-        pass
+        self.ordered = True
+        return (f"You've ordered pizza {self.name} prepared with "
+                f"{', '.join([f'{k}: {v}' for k, v in self.ingredients.items()])}"
+                f" and the price will be {self.price}lv.")
