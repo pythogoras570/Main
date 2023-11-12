35b9d340a377f83a3d47879066f212db1c742b7e
pythogoras570 <133265951+pythogoras570@users.noreply.github.com>
2023-11-12 21:55:35 +0200

    Added full code.

--- OOP/Lectures/encapsulation/profile.py
+++ OOP/Lectures/encapsulation/profile.py
@@ -20,10 +20,13 @@ class Profile:
 
     @password.setter
     def password(self, password):
-        if len(password) < 8:
-            raise ValueError('The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.')
-        else:
+        is_valid_length = len(password) >= 8
+        is_upper_case = any([char for char in password if char.isupper()])
+        is_digit_present = any([char for char in password if char.isdigit()])
+        if is_valid_length and is_upper_case and is_digit_present:
             self.__password = password
+        else:
+            raise ValueError('The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.')
 
     def __str__(self):
-        return f'You have a profile with username: "{self.username}" and password: {len(self.password)*"*"}.'
+        return f'You have a profile with username: "{self.username}" and password: {len(self.password)*"*"}'
