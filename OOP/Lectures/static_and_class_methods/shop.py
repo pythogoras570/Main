8618dc8a25cb9edc79e5b49eaa2bfd5ba62ce514
pythogoras570 <133265951+pythogoras570@users.noreply.github.com>
2023-11-29 22:44:22 +0200

    Added full code.

---
+++ OOP/Lectures/static_and_class_methods/shop.py
@@ -0,0 +1,20 @@
+class Shop:
+    def __init__(self, name: str, type: str, capacity: int):
+        self.name = name
+        self.type = type
+        self.capacity = capacity
+        self.items = {}
+
+    @classmethod
+    def small_shop(cls, name, type):
+        return cls(name, type, 10)
+
+    def add_item(self, item_name):
+
+        pass
+
+    def remove_item(self):
+        pass
+
+    def __repr__(self):
+        pass
