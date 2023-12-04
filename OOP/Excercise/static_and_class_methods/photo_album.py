beccd584ba480616f87c613b2ef166e82dd144fe
pythogoras570 <133265951+pythogoras570@users.noreply.github.com>
2023-12-04 23:28:07 +0200

    Added full code.

---
+++ OOP/Excercise/static_and_class_methods/photo_album.py
@@ -0,0 +1,13 @@
+class PhotoAlbum:
+    def __init__(self, pages: int):
+        self.pages = pages
+        self.photos = []
+
+    def from_photos_count(self, photos_count: int):
+        pass
+
+    def add_photo(self, label: str):
+        pass
+
+    def display(self):
+        pass
